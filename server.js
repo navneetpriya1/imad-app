f4eb062f6d91edbc4923eb57f37ab40e1add5539
Navneet Priya <sroy7766@gmail.com>
2017-08-23 01:01:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/ui/madi.png', function (req, res) {
 var counter=0;
 app.get('/counter',function(req, res){
    counter= counter+1;
-   res.send(counter.tostring());
+   res.send(counter.toString());
 });
 
 app.get('/article-one', function(req, res){
