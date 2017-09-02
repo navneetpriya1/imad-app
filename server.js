50bde5574d7afd0b6eec2340779fb155a0ec4b5c
Navneet Priya <sroy7766@gmail.com>
2017-09-02 23:38:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var article{
-    article-one
-}
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
