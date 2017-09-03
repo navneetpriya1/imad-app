6c913cdbc857249890114b733f1d47c5c62becd0
Navneet Priya <sroy7766@gmail.com>
2017-09-03 21:39:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,7 @@ app.get('/counter',function(req, res){
 });
 
 app.get('/:articleName', function(req, res){
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
