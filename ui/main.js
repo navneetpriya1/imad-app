2b4c0681892e1f385701c251928f061a9271ff02
Navneet Priya <sroy7766@gmail.com>
2017-09-04 05:12:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,3 +7,14 @@ button.onclick = function()  {
    var span= document.getElementById('count');
    span.innerHTML = counter.toString();
 };
+
+var img = document.getElementById('img-medium');
+
+function moveRight(){
+    marginLeft= marginLeft + 5;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+img.onclick = function(){
+    var interval = setInterval(moveRight + 100);
+};
