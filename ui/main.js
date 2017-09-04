a5ffc07e71c473a667bf1d48d815ea0ffabc91c1
Navneet Priya <sroy7766@gmail.com>
2017-09-05 01:29:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,17 @@ var button = document.getElementById('counter');
 var counter = 0;
 
 button.onclick = function()  {
-   counter= counter + 1;
-   var span= document.getElementById('count');
-   span.innerHTML = counter.toString();
+    var request = XMLHttpRequest();
+    request.onreadystatechange = function(){
+      if(this.readyState === 4 && this.status === 200){
+        counter = request.responseText;
+        var span= document.getElementById('count');
+        span.innerHTML = counter.toString();
+      }
+    };
+   request.open('GET','http://sroy7766.imad.hasura-app.io/',true);
+   request.send(null);
+  
 };
 
 var imge = document.getElementById('madi');
