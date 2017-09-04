8ce090489e2a7016c833091a64c976e30bd57936
Navneet Priya <sroy7766@gmail.com>
2017-09-04 06:33:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,15 +8,15 @@ var counter = 0;
    span.innerHTML = counter.toString();
 };*/
 
-var img = document.getElementById('madi');
+var imge = document.getElementById('madi');
 
 var marginLeft = 0;
 
 function moveRight(){
     marginLeft= marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
+    imge.style.marginLeft = marginLeft + 'px';
 }
 
-img.onclick = function(){
+imge.onclick = function(){
     var interval = setInterval(moveRight + 100);
 };
