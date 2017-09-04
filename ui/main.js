b77274ac723ba1033e03bde03a460797d79dad5c
Navneet Priya <sroy7766@gmail.com>
2017-09-04 07:21:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,11 @@ console.log('loaded');
 var button = document.getElementById('counter');
 var counter = 0;
 
-/*button.onclick = function()  {
+button.onclick = function()  {
    counter= counter + 1;
    var span= document.getElementById('count');
    span.innerHTML = counter.toString();
-};*/
+};
 
 var imge = document.getElementById('madi');
 
