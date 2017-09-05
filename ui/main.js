1c15da1e0ed35c7605771e0f2a1d3b2ca7ed61f6
Navneet Priya <sroy7766@gmail.com>
2017-09-06 01:05:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,14 +30,15 @@ imge.onclick = function(){
     var interval = setInterval(moveRight + 100);
 };
 
-var nameInput = document.getElementById('name');
+var nameInput = document.getElementById('namee');
 var name = nameInput.value;
 var submit = document.getElementById('submit-btn');
 submit.onclick = function(){
-    var name = [name1,name2,name3,name4];
+    var names = [name1,name2,name3,name4];
     var list = "";
-    for(var i=0 ; i < name.length ; i++){
+    for(var i=0 ; i < names.length ; i++){
         list+= '<li>'+name[i]+'</li>';
     }
+    var ul = document.getElementById('nameList');
     
 };
