6dc8d65506c7df767e8b0890046994cd726b2a0d
Navneet Priya <sroy7766@gmail.com>
2017-09-06 01:07:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,6 +40,6 @@ submit.onclick = function(){
         list+= '<li>'+name[i]+'</li>';
     }
     var ul = document.getElementById('nameList');
-    ul.innerHTML = nameList.value;
+    ul.innerHTML = list;
     
 };
